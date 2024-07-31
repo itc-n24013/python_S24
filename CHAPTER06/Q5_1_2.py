34065d10ec045e892573f3edd87232c1f39b5101
n24013 <n24013@std.it-college.ac.jp>
2024-07-31 07:46:22 +0000

    7月31日

---
+++ CHAPTER06/Q5_1_2.py
@@ -0,0 +1,5 @@
+stack = []
+stack.append(1)
+stack.append(2)
+for i in range(2):
+    print(stack.pop())
---
+++ CHAPTER06/Q5_6_2.py
@@ -0,0 +1,4 @@
+A = {x for x in range(21)}
+B = {i for i in range(21) if i % 2 == 0}
+C = (A - B)
+print(C)
